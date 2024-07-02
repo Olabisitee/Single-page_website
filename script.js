72977de87d0b0ee485efb4728fede7e92fb9e84a
Olabisitee <oladepoolabisitee2001@gmail.com>
2024-07-02 02:35:01 -0700

    initial commit

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function updateTimeAndDay() {
   const now = new Date();
   const utcTime = now.toUTCString();
-  const dayOfWeek = now.toLocaleString("en-US", { weekday: "long" });
+  const dayOfWeek = now.toLocaleString("en-US", {
+    weekday: "long",
+    timeZone: "UTC",
+  });
 
   document.getElementById("utc-time").textContent = utcTime;
   document.getElementById("current-day").textContent = dayOfWeek;
